c50cc9bf777b47f5a09a2e5cd1fd004e0ab1bcfe
Sebastião Felix <54052660+Raposa-Dev@users.noreply.github.com>
2022-02-18 01:16:45 -0300

    Add index.html, main.js - Create odd Even

---
+++ odd_even/main.js
@@ -0,0 +1,19 @@
+const students = 10;
+
+for (let number = 0; number <= 10; number++){
+
+    
+    if(number == 0) {
+
+        console.log(+number+ " Zero ")
+
+    } else if (number % 2 == 0){
+
+        console.log(+number+ ' Even ');
+
+    }else if (number % 2 != 0){
+
+        console.log(+number+ ' Odd ');
+    };
+
+};
